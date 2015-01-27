56e3e31cc542e525d2f939dc0e472afeb7c0f6c9
Brandon2010 <wxbrandon2010@gmail.com>
2015-01-27 17:49:56 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/GamePlay.h
@@ -0,0 +1,13 @@
+//
+//  GamePlay.h
+//  PeevedPenguins
+//
+//  Created by Brandon on 1/27/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface GamePlay : CCNode
+
+@end
